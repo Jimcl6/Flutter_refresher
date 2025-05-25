1114ecc3a5023b245f5b9d2ab12804896f062ece
Jed Llorente <jedllorente@gmail.com>
2025-05-25 12:06:09 +0800

    commit changes to mi_card_app

--- mi_card_app/lib/main.dart
+++ mi_card_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,28 +15,47 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            verticalDirection: VerticalDirection.down,
-            children: [
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.white,
-                child: const Text('Hello World!'),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
-                child: const Text('Hello World!'),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.red,
-                child: const Text('Hello World!'),
-              ),
-            ],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // profile picture
+                CircleAvatar(
+                  radius: 50.0,
+                  foregroundImage: NetworkImage(
+                    'https://media.licdn.com/dms/image/v2/C4D03AQFmhBaNrxgh3Q/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1517604351326?e=1753920000&v=beta&t=Rbd7eZo5e83DmkCWn8_TK8VzyVKH8OQTcMnCKiF5TGw',
+                  ),
+                ),
+
+                // name
+                Text(
+                  'Jed Llorente',
+                  style: GoogleFonts.lobster(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+
+                // profession
+                Text(
+                  'Software Engineer'.toUpperCase(),
+                  style: GoogleFonts.sourceSans3(
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                      letterSpacing: 2.5,
+                    ),
+                  ),
+                ),
+                // horizontal line
+                SizedBox(
+                  height: 20.0,
+                  width: 250.0,
+                  child: Divider(thickness: 0.5, color: Colors.white),
+                ),
+                // job title
+              ],
+            ),
           ),
         ),
       ),
