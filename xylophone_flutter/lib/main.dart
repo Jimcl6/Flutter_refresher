873cd65626814229f4386e93157b593c2384a9b1
Jimcl6 <jedllorente@gmail.com>
2025-06-05 17:29:43 +0800

    commit changes made to main.dart

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -9,6 +9,11 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: SafeArea(child: Container())));
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(child: Container()),
+      ),
+    );
   }
 }
