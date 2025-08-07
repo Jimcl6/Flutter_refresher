519151d3f209ccf97542c3544439f6b4f26c9eea
Jimcl6 <jedllorente@gmail.com>
2025-08-07 17:31:18 +0800

    commit redoing quiz_app_remake

--- quiz_app_remake/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:quiz_app_remake/quiz.dart';
-
-void main() {
-  runApp(const QuizPage());
-}
---
+++ quizz_app_remake/android/app/src/main/kotlin/com/example/quizz_app_remake/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quizz_app_remake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
